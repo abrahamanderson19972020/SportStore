79b494138e67e43edc88ab2923967532c23f864c
Abraham Anderson <abraham.anderson83@gmail.com>
2023-04-27 00:42:07 +0200

    fourth commit

--- src/app/store/product/product.component.ts
+++ src/app/store/product/product.component.ts
@@ -8,14 +8,39 @@ import { StaticDataSource } from 'src/app/models/model/static.datasource';
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
+  selectedCategory: string | undefined;
+  productsPerPage: number = 4;
+  selectedPage: number = 1;
   products: Product[] = [];
+  allProducts: Product[] = [];
   categories: string[] = [];
   constructor(private dataService: StaticDataSource) {}
   ngOnInit(): void {
     this.dataService.getProducts().subscribe((res) => {
+      this.allProducts = res;
+      let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+      res.slice(pageIndex, pageIndex + this.productsPerPage);
       this.products = res;
     });
     this.categories = this.dataService.getAllCategories();
     console.log(this.categories);
+  }
+
+  changeCategory(category?: string) {
+    this.selectedCategory = category;
+    this.products = this.dataService.getProductByCategory(category!);
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+  get pageNumbers(): number[] {
+    return Array(Math.ceil(this.products?.length / this.productsPerPage))
+      .fill(0)
+      .map((x, i) => i + 1);
   }
 }
